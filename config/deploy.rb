2843b5b4ec68bd1dc69e4a14490af3c8e4da169d
takumi-915 <59188083+takumi-915@users.noreply.github.com>
2020-02-25 22:56:29 +0900

    アプリ名変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,10 +3,10 @@
 lock '3.11.2'
 
 # Capistranoのログの表示に利用する
-set :application, 'taskapp'
+set :application, 'book-management'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:takumi-915/taskapp.git'
+set :repo_url,  'git@github.com:takumi-915/book-management.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
@@ -16,7 +16,7 @@ set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/taskapp2.pem'] 
+                  keys: ['~/.ssh/book-management2.pem'] 
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
